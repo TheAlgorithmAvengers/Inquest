d1cb46ae6f24f60402b2cbcf0da6fd136627d95f
Algorithm Avengers <srm.algorithmavengers@gmail.com>
2025-11-19 18:09:44 +0530

    Delete extra directory

--- extra/TTS and STT.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something...")
-    audio = r.listen(source)
-
-text = r.recognize_google(audio)
-print("You said:", text)
